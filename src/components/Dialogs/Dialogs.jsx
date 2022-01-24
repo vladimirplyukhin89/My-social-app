a70b86fdefc11e126c6c572eaf6adb3a9eb57f82
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2022-01-23 20:53:16 -1100

    Updated dialogs page and UI/BLL

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,37 +2,62 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
 
+// подкомпонента в Dialogs
+const DialogItem = (props) => {
+    // наш url из props
+    let path = `/dialogs/${props.id}`;
+
+    return (
+        <div className={`${classes.dialog} ${classes.active}`}>
+            <NavLink to={path}>
+                {props.name}
+            </NavLink>
+        </div>
+    )
+}
+
+const DialogMessage = (props) => {
+
+    return (
+        <div className={classes.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "John" },
+        { id: 3, name: "Tony" },
+        { id: 4, name: "Mike" },
+    ]
+
+    let messagesData = [
+        { id: 1, message: "Hey, bro" },
+        { id: 2, message: "How's it going?" },
+        { id: 3, message: "Everything's ok" },
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={`${classes.dialog} ${classes.active}`}>
-                    <NavLink to='/dialogs/1'>
-                        Kris
-                    </NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/2'>
-                        John
-                    </NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/3'>
-                        Tony
-                    </NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialog4'>
-                        Mike
-                    </NavLink>
-                </div>
+
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+
             </div>
 
             <div className={classes.messages} >
-                <div className={classes.message}>Hey</div>
-                <div className={classes.message}>whats up</div>
-                <div className={classes.message}>Come on</div>
+                <DialogMessage message={messagesData[0].message} />
+
+                <DialogMessage message={messagesData[1].message} />
+
+                <DialogMessage message={messagesData[2].message} />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,15 +3,29 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postsData = [
+        { id: 1, message: "How's it going, bro?", likesCount: 15 },
+        { id: 2, message: "When do you leave the city?", likesCount: 20 },
+    ]
+
     return (
-        <div>
+        <div className={classes.postsBlock}>
+            <h3>My posts</h3>
+
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
-            <div >
-                <Post message="How's it going, bro?" likesCount="15" />
-                <Post message="When do you leave the city?" likesCount="20" />
+
+            <div className={classes.posts}>
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
             </div>
         </div>
     )
