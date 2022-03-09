2e8122fc7686f4577b185ebe44e685e9dc134ff0
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2022-03-08 20:49:52 -1100

    Added axios library and GET quiry

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,15 @@
+import axios from 'axios';
 import React from 'react';
 import classes from './Users.module.css';
+import * as a from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I am a boss',
-                location: { city: 'Minsk', country: 'Belarus' }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                followed: true,
-                fullName: 'Sasha',
-                status: 'I am a boss too',
-                location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                followed: false,
-                fullName: 'Andrew',
-                status: 'I am a boss too',
-                location: { city: 'Kiev', country: 'Ukraine' }
-            }
-        ]
-        )
+        a.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
 
     return <div>
@@ -37,7 +17,7 @@ let Users = (props) => {
             props.users.map(user => <div key={user.id}>
                 <span>
                     <div>
-                        <img src={user.photoUrl} className={classes.userPhoto} />
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto} className={classes.userPhoto} />
                     </div>
                     <div>
                         {user.followed
@@ -48,12 +28,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{user.fullName}</div>
+                        <div>{user.name}</div>
                         <div>{user.status}</div>
                     </span>
                     <span>
-                        <div>{user.location.city}</div>
-                        <div>{user.location.country}</div>
+                        <div>{'user.location.city'}</div>
+                        <div>{'user.location.country'}</div>
                     </span>
                 </span>
             </div>)
